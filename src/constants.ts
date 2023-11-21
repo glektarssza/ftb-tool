a360f3b7eb832a97c0aee75a3b4e82389c7ffdc4
G'lek Tarssza <glek@glektarssza.com>
2023-11-21 13:56:47 -0700

    Added a bunch of constants.

--- src/constants.ts
+++ src/constants.ts
@@ -1,9 +1,34 @@
-/** 
+/**
  * The name of the application.
  */
 export const APP_NAME = 'ftb-tool';
 
-/** 
+/**
  * The version of the application.
  */
 export const APP_VERSION = '0.0.0';
+
+/**
+ * The default user agent to use when making network requests.
+ */
+export const DEFAULT_USER_AGENT = `${APP_NAME}/${APP_VERSION} NodeJS/${process.version}`;
+
+/**
+ * The default base URL of the Feed the Beast API.
+ */
+export const DEFAULT_FTB_API_BASE_URL = 'https://api.modpacks.ch';
+
+/**
+ * The default base URL of the CurseForge API.
+ */
+export const DEFAULT_FLAME_API_BASE_URL = 'https://api.curseforge.com/v1';
+
+/**
+ * The default timeout on network requests, in seconds.
+ */
+export const DEFAULT_NET_TIMEOUT = 10;
+
+/**
+ * The default maximum number of parallel network requests that can be made.
+ */
+export const DEFAULT_NET_REQUEST_LIMIT = 3;
