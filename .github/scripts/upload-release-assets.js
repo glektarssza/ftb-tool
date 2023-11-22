57f8b4f0dc2fbd0cd5779b759402a6d6ae43b617
G'lek Tarssza <glek@glektarssza.com>
2023-11-22 15:18:14 -0700

    Still trying to fix release asset uploading.

--- .github/scripts/upload-release-assets.js
+++ .github/scripts/upload-release-assets.js
@@ -1,14 +1,47 @@
 const fs = require('node:fs/promises');
 const path = require('node:path');
 
-module.exports = async ({ github, context, core }) => {
-    const { owner, repo } = context.repo;
-    const items = (await fs.readdir('./dist/', {
+/**
+ * Get all items in a directory recursively.
+ *
+ * @param {string} path The path to the directory to scan.
+ *
+ * @returns {Promise<fs.Dirent[]>} A promise that resolves to an array of
+ * `fs.Dirent` objects.
+ */
+async function getItemsRecursive(path) {
+    const items = await fs.readdir(path, {
         withFileTypes: true,
         encoding: 'utf-8'
-    })).filter((item) => item.isFile());
-    core.info(`Uploading ${items.length} assets to release ${context.payload.release.id}...`);
-    for (const artifact of items) {
+    });
+    const results = [];
+    for (const item of items) {
+        if (item.isDirectory()) {
+            const subItems = await getItemsRecursive(path.join(item.path, item.name));
+            results.push(...subItems);
+        } else {
+            results.push(item);
+        }
+    }
+    return results;
+}
+
+/**
+ * Upload all files in a directory to a release.
+ *
+ * @param {object} param0 The GitHub context object.
+ * @param {string} artifactDirectory The path to the directory containing the
+ * artifacts to upload.
+ *
+ * @returns {Promise<void>} A promise that resolves when all assets have been
+ * uploaded.
+ */
+module.exports = async ({ github, context, core }, artifactDirectory) => {
+    const { owner, repo } = context.repo;
+    const items = await getItemsRecursive(artifactDirectory);
+    const assets = items.filter((item) => item.isFile());
+    core.info(`Uploading ${assets.length} assets to release ${context.payload.release.id}...`);
+    for (const artifact of assets) {
         const artifactPath = path.join(artifact.path, artifact.name);
         core.info(`Reading ${artifact.name} from ${artifactPath}...`);
         const data = await fs.readFile(artifactPath);
