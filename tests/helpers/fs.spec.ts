c43d72f5bb437e3d30946e7159c9e41f3e59c3ad
G'lek Tarssza <glek@glektarssza.com>
2023-11-27 11:15:52 -0700

    Added some more tests.

--- tests/helpers/fs.spec.ts
+++ tests/helpers/fs.spec.ts
@@ -162,11 +162,471 @@ describe('module:helpers.fs', () => {
             expect(r).to.be.false;
         });
     });
-    describe('.isDirectory', () => {});
-    describe('.isBlockDevice', () => {});
-    describe('.isCharacterDevice', () => {});
-    describe('.isSocket', () => {});
-    describe('.isSymbolicLink', () => {});
+    describe('.isDirectory', () => {
+        let existsStub: SinonStub<
+            Parameters<typeof fsHelper.exists>,
+            ReturnType<typeof fsHelper.exists>
+        >;
+        let statStub: SinonStub<
+            Parameters<typeof fs.stat>,
+            ReturnType<typeof fs.stat>
+        >;
+        before(() => {
+            existsStub = stub(fsHelper, 'exists');
+            statStub = stub(fs, 'stat');
+        });
+        beforeEach(() => {
+            existsStub.reset();
+            statStub.reset();
+            existsStub.callThrough();
+            statStub.callThrough();
+        });
+        after(() => {
+            existsStub.restore();
+            statStub.restore();
+        });
+        it('should call `exists` on the path', async () => {
+            //-- Given
+            const path = fake.system.filePath();
+
+            //-- When
+            await fsHelper.isDirectory(path);
+
+            //-- Then
+            expect(existsStub).to.have.been.calledOnceWith(path);
+        });
+        it('should call `fs.stat` on the path', async () => {
+            //-- Given
+            const path = fake.system.filePath();
+            existsStub.withArgs(path).resolves(true);
+            statStub.withArgs(path).resolves({
+                isDirectory() {
+                    return true;
+                }
+            } as Stats);
+
+            //-- When
+            await fsHelper.isDirectory(path);
+
+            //-- Then
+            expect(statStub).to.have.been.calledOnceWith(path);
+        });
+        it('should return `true` if the path is a directory', async () => {
+            //-- Given
+            const path = fake.system.filePath();
+            existsStub.withArgs(path).resolves(true);
+            statStub.withArgs(path).resolves({
+                isDirectory() {
+                    return true;
+                }
+            } as Stats);
+
+            //-- When
+            const r = await fsHelper.isDirectory(path);
+
+            //-- Then
+            expect(r).to.be.true;
+        });
+        it('should return `false` if the path exists but is not a file', async () => {
+            //-- Given
+            const path = fake.system.filePath();
+            existsStub.withArgs(path).resolves(true);
+            statStub.withArgs(path).resolves({
+                isDirectory() {
+                    return false;
+                }
+            } as Stats);
+
+            //-- When
+            const r = await fsHelper.isDirectory(path);
+
+            //-- Then
+            expect(r).to.be.false;
+        });
+        it('should return `false` if the path does not exist', async () => {
+            //-- Given
+            const path = fake.system.filePath();
+            existsStub.withArgs(path).resolves(false);
+
+            //-- When
+            const r = await fsHelper.isDirectory(path);
+
+            //-- Then
+            expect(r).to.be.false;
+        });
+    });
+    describe('.isBlockDevice', () => {
+        let existsStub: SinonStub<
+            Parameters<typeof fsHelper.exists>,
+            ReturnType<typeof fsHelper.exists>
+        >;
+        let statStub: SinonStub<
+            Parameters<typeof fs.stat>,
+            ReturnType<typeof fs.stat>
+        >;
+        before(() => {
+            existsStub = stub(fsHelper, 'exists');
+            statStub = stub(fs, 'stat');
+        });
+        beforeEach(() => {
+            existsStub.reset();
+            statStub.reset();
+            existsStub.callThrough();
+            statStub.callThrough();
+        });
+        after(() => {
+            existsStub.restore();
+            statStub.restore();
+        });
+        it('should call `exists` on the path', async () => {
+            //-- Given
+            const path = fake.system.filePath();
+
+            //-- When
+            await fsHelper.isBlockDevice(path);
+
+            //-- Then
+            expect(existsStub).to.have.been.calledOnceWith(path);
+        });
+        it('should call `fs.stat` on the path', async () => {
+            //-- Given
+            const path = fake.system.filePath();
+            existsStub.withArgs(path).resolves(true);
+            statStub.withArgs(path).resolves({
+                isBlockDevice() {
+                    return true;
+                }
+            } as Stats);
+
+            //-- When
+            await fsHelper.isBlockDevice(path);
+
+            //-- Then
+            expect(statStub).to.have.been.calledOnceWith(path);
+        });
+        it('should return `true` if the path is a file', async () => {
+            //-- Given
+            const path = fake.system.filePath();
+            existsStub.withArgs(path).resolves(true);
+            statStub.withArgs(path).resolves({
+                isBlockDevice() {
+                    return true;
+                }
+            } as Stats);
+
+            //-- When
+            const r = await fsHelper.isBlockDevice(path);
+
+            //-- Then
+            expect(r).to.be.true;
+        });
+        it('should return `false` if the path exists but is not a file', async () => {
+            //-- Given
+            const path = fake.system.filePath();
+            existsStub.withArgs(path).resolves(true);
+            statStub.withArgs(path).resolves({
+                isBlockDevice() {
+                    return false;
+                }
+            } as Stats);
+
+            //-- When
+            const r = await fsHelper.isBlockDevice(path);
+
+            //-- Then
+            expect(r).to.be.false;
+        });
+        it('should return `false` if the path does not exist', async () => {
+            //-- Given
+            const path = fake.system.filePath();
+            existsStub.withArgs(path).resolves(false);
+
+            //-- When
+            const r = await fsHelper.isBlockDevice(path);
+
+            //-- Then
+            expect(r).to.be.false;
+        });
+    });
+    describe('.isCharacterDevice', () => {
+        let existsStub: SinonStub<
+            Parameters<typeof fsHelper.exists>,
+            ReturnType<typeof fsHelper.exists>
+        >;
+        let statStub: SinonStub<
+            Parameters<typeof fs.stat>,
+            ReturnType<typeof fs.stat>
+        >;
+        before(() => {
+            existsStub = stub(fsHelper, 'exists');
+            statStub = stub(fs, 'stat');
+        });
+        beforeEach(() => {
+            existsStub.reset();
+            statStub.reset();
+            existsStub.callThrough();
+            statStub.callThrough();
+        });
+        after(() => {
+            existsStub.restore();
+            statStub.restore();
+        });
+        it('should call `exists` on the path', async () => {
+            //-- Given
+            const path = fake.system.filePath();
+
+            //-- When
+            await fsHelper.isCharacterDevice(path);
+
+            //-- Then
+            expect(existsStub).to.have.been.calledOnceWith(path);
+        });
+        it('should call `fs.stat` on the path', async () => {
+            //-- Given
+            const path = fake.system.filePath();
+            existsStub.withArgs(path).resolves(true);
+            statStub.withArgs(path).resolves({
+                isCharacterDevice() {
+                    return true;
+                }
+            } as Stats);
+
+            //-- When
+            await fsHelper.isCharacterDevice(path);
+
+            //-- Then
+            expect(statStub).to.have.been.calledOnceWith(path);
+        });
+        it('should return `true` if the path is a file', async () => {
+            //-- Given
+            const path = fake.system.filePath();
+            existsStub.withArgs(path).resolves(true);
+            statStub.withArgs(path).resolves({
+                isCharacterDevice() {
+                    return true;
+                }
+            } as Stats);
+
+            //-- When
+            const r = await fsHelper.isCharacterDevice(path);
+
+            //-- Then
+            expect(r).to.be.true;
+        });
+        it('should return `false` if the path exists but is not a file', async () => {
+            //-- Given
+            const path = fake.system.filePath();
+            existsStub.withArgs(path).resolves(true);
+            statStub.withArgs(path).resolves({
+                isCharacterDevice() {
+                    return false;
+                }
+            } as Stats);
+
+            //-- When
+            const r = await fsHelper.isCharacterDevice(path);
+
+            //-- Then
+            expect(r).to.be.false;
+        });
+        it('should return `false` if the path does not exist', async () => {
+            //-- Given
+            const path = fake.system.filePath();
+            existsStub.withArgs(path).resolves(false);
+
+            //-- When
+            const r = await fsHelper.isCharacterDevice(path);
+
+            //-- Then
+            expect(r).to.be.false;
+        });
+    });
+    describe('.isSocket', () => {
+        let existsStub: SinonStub<
+            Parameters<typeof fsHelper.exists>,
+            ReturnType<typeof fsHelper.exists>
+        >;
+        let statStub: SinonStub<
+            Parameters<typeof fs.stat>,
+            ReturnType<typeof fs.stat>
+        >;
+        before(() => {
+            existsStub = stub(fsHelper, 'exists');
+            statStub = stub(fs, 'stat');
+        });
+        beforeEach(() => {
+            existsStub.reset();
+            statStub.reset();
+            existsStub.callThrough();
+            statStub.callThrough();
+        });
+        after(() => {
+            existsStub.restore();
+            statStub.restore();
+        });
+        it('should call `exists` on the path', async () => {
+            //-- Given
+            const path = fake.system.filePath();
+
+            //-- When
+            await fsHelper.isSocket(path);
+
+            //-- Then
+            expect(existsStub).to.have.been.calledOnceWith(path);
+        });
+        it('should call `fs.stat` on the path', async () => {
+            //-- Given
+            const path = fake.system.filePath();
+            existsStub.withArgs(path).resolves(true);
+            statStub.withArgs(path).resolves({
+                isSocket() {
+                    return true;
+                }
+            } as Stats);
+
+            //-- When
+            await fsHelper.isSocket(path);
+
+            //-- Then
+            expect(statStub).to.have.been.calledOnceWith(path);
+        });
+        it('should return `true` if the path is a file', async () => {
+            //-- Given
+            const path = fake.system.filePath();
+            existsStub.withArgs(path).resolves(true);
+            statStub.withArgs(path).resolves({
+                isSocket() {
+                    return true;
+                }
+            } as Stats);
+
+            //-- When
+            const r = await fsHelper.isSocket(path);
+
+            //-- Then
+            expect(r).to.be.true;
+        });
+        it('should return `false` if the path exists but is not a file', async () => {
+            //-- Given
+            const path = fake.system.filePath();
+            existsStub.withArgs(path).resolves(true);
+            statStub.withArgs(path).resolves({
+                isSocket() {
+                    return false;
+                }
+            } as Stats);
+
+            //-- When
+            const r = await fsHelper.isSocket(path);
+
+            //-- Then
+            expect(r).to.be.false;
+        });
+        it('should return `false` if the path does not exist', async () => {
+            //-- Given
+            const path = fake.system.filePath();
+            existsStub.withArgs(path).resolves(false);
+
+            //-- When
+            const r = await fsHelper.isSocket(path);
+
+            //-- Then
+            expect(r).to.be.false;
+        });
+    });
+    describe('.isSymbolicLink', () => {
+        let existsStub: SinonStub<
+            Parameters<typeof fsHelper.exists>,
+            ReturnType<typeof fsHelper.exists>
+        >;
+        let statStub: SinonStub<
+            Parameters<typeof fs.stat>,
+            ReturnType<typeof fs.stat>
+        >;
+        before(() => {
+            existsStub = stub(fsHelper, 'exists');
+            statStub = stub(fs, 'stat');
+        });
+        beforeEach(() => {
+            existsStub.reset();
+            statStub.reset();
+            existsStub.callThrough();
+            statStub.callThrough();
+        });
+        after(() => {
+            existsStub.restore();
+            statStub.restore();
+        });
+        it('should call `exists` on the path', async () => {
+            //-- Given
+            const path = fake.system.filePath();
+
+            //-- When
+            await fsHelper.isSymbolicLink(path);
+
+            //-- Then
+            expect(existsStub).to.have.been.calledOnceWith(path);
+        });
+        it('should call `fs.stat` on the path', async () => {
+            //-- Given
+            const path = fake.system.filePath();
+            existsStub.withArgs(path).resolves(true);
+            statStub.withArgs(path).resolves({
+                isSymbolicLink() {
+                    return true;
+                }
+            } as Stats);
+
+            //-- When
+            await fsHelper.isSymbolicLink(path);
+
+            //-- Then
+            expect(statStub).to.have.been.calledOnceWith(path);
+        });
+        it('should return `true` if the path is a file', async () => {
+            //-- Given
+            const path = fake.system.filePath();
+            existsStub.withArgs(path).resolves(true);
+            statStub.withArgs(path).resolves({
+                isSymbolicLink() {
+                    return true;
+                }
+            } as Stats);
+
+            //-- When
+            const r = await fsHelper.isSymbolicLink(path);
+
+            //-- Then
+            expect(r).to.be.true;
+        });
+        it('should return `false` if the path exists but is not a file', async () => {
+            //-- Given
+            const path = fake.system.filePath();
+            existsStub.withArgs(path).resolves(true);
+            statStub.withArgs(path).resolves({
+                isSymbolicLink() {
+                    return false;
+                }
+            } as Stats);
+
+            //-- When
+            const r = await fsHelper.isSymbolicLink(path);
+
+            //-- Then
+            expect(r).to.be.false;
+        });
+        it('should return `false` if the path does not exist', async () => {
+            //-- Given
+            const path = fake.system.filePath();
+            existsStub.withArgs(path).resolves(false);
+
+            //-- When
+            const r = await fsHelper.isSymbolicLink(path);
+
+            //-- Then
+            expect(r).to.be.false;
+        });
+    });
     describe('.isReadable', () => {});
     describe('.createReadableStream', () => {});
     describe('.createWriteableStream', () => {});
