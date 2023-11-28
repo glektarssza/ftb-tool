ed650a558290ff43784ef5cc1c7e66e135b2b8a2
G'lek Tarssza <glek@glektarssza.com>
2023-11-28 06:37:29 -0700

    Fixed exports.

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,36 +1,43 @@
-export * from './logging';
-export * from './fs';
-export * from './net';
+import fs from './fs';
+import * as logging from './logging';
+import * as net from './net';
 
-/**
- * Check if a value is `null`.
- *
- * @param value - The value to check.
- *
- * @returns Whether the value is `null`.
- */
-export function isNull(value: unknown): value is null {
-    return value === null;
-}
+const exported = {
+    ...fs,
+    ...logging,
+    ...net,
+    /**
+     * Check if a value is `null`.
+     *
+     * @param value - The value to check.
+     *
+     * @returns Whether the value is `null`.
+     */
+    isNull: (value: unknown): value is null => {
+        return value === null;
+    },
 
-/**
- * Check if a value is `undefined`.
- *
- * @param value - The value to check.
- *
- * @returns Whether the value is `undefined`.
- */
-export function isUndefined(value: unknown): value is undefined {
-    return value === undefined;
-}
+    /**
+     * Check if a value is `undefined`.
+     *
+     * @param value - The value to check.
+     *
+     * @returns Whether the value is `undefined`.
+     */
+    isUndefined: (value: unknown): value is undefined => {
+        return value === undefined;
+    },
 
-/**
- * Check if a value is `null` or `undefined`.
- *
- * @param value - The value to check.
- *
- * @returns Whether the value is `null` or `undefined`.
- */
-export function isNil(value: unknown): value is null | undefined {
-    return isNull(value) || isUndefined(value);
-}
+    /**
+     * Check if a value is `null` or `undefined`.
+     *
+     * @param value - The value to check.
+     *
+     * @returns Whether the value is `null` or `undefined`.
+     */
+    isNil: (value: unknown): value is null | undefined => {
+        return exported.isNull(value) || exported.isUndefined(value);
+    }
+};
+
+export = exported;
