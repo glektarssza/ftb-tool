48a76b3d31a25b3d9b6ab66530ca797045241824
G'lek Tarssza <glek@glektarssza.com>
2023-11-30 14:31:38 -0700

    Bump version to v0.2.0.

--- src/constants.ts
+++ src/constants.ts
@@ -6,7 +6,7 @@ export const APP_NAME = 'ftb-tool';
 /**
  * The version of the application.
  */
-export const APP_VERSION = '0.1.0';
+export const APP_VERSION = '0.2.0';
 
 /**
  * Whether to log verbosely by default.
