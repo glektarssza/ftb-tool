2d003a66e81ede4a52351d1959aa37ec7721103d
G'lek Tarssza <glek@glektarssza.com>
2023-12-01 08:24:17 -0700

    Stubbed out networking helper tests.

---
+++ tests/helpers/net.spec.ts
@@ -0,0 +1,31 @@
+//-- NodeJS
+
+//-- NPM Packages
+import chai from 'chai';
+import {} from 'sinon';
+import sinonChai from 'sinon-chai';
+import {en, en_US, base, Faker} from '@faker-js/faker';
+
+//-- Project Code
+
+//-- Test Utils
+import {parseOptionalEnvInteger} from '../utils';
+
+chai.use(sinonChai);
+
+/**
+ * The data faker instance for these tests.
+ */
+const fake = new Faker({
+    locale: [en, en_US, base]
+});
+
+describe('module:helpers.fs', () => {
+    before(() => {
+        console.debug(
+            `Using "${fake.seed(
+                parseOptionalEnvInteger('TESTS_FAKER_SEED') ?? undefined
+            )}"`
+        );
+    });
+});
