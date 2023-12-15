95a5237fee9160293080400b433408c089daad64
G'lek Tarssza <glek@glektarssza.com>
2023-12-15 12:37:30 -0700

    Added isFile tests
    Moment of magic, this has failed before due to ESM stuff.

--- tests/lib/fs.spec.ts
+++ tests/lib/fs.spec.ts
@@ -1,4 +1,5 @@
 //-- NodeJS
+import fs from 'node:fs';
 import fsp from 'node:fs/promises';
 
 //-- NPM Packages
@@ -11,7 +12,7 @@ import {en, en_US, base, Faker} from '@faker-js/faker';
 import testModule from '@src/lib/fs';
 
 //-- Test Utils
-import {StubbedProperty} from '../types';
+import {StubbedObject, StubbedProperty} from '../types';
 import {parseOptionalEnvInteger} from '../utils';
 
 chai.use(sinonChai);
@@ -62,6 +63,72 @@ describe('module:lib.fs', () => {
 
             //-- When
             const r = await testModule.exists(p);
+
+            //-- Then
+            expect(r).to.be.false;
+        });
+    });
+    describe('.isFile', () => {
+        let existsStub: StubbedProperty<typeof testModule, 'exists'>;
+        let statStub: StubbedProperty<typeof fsp, 'stat'>;
+        let isFileStub: StubbedProperty<fs.Stats, 'isFile'>;
+        let statsInstance: Partial<StubbedObject<fs.Stats>>;
+        before(() => {
+            existsStub = stub(testModule, 'exists');
+            statStub = stub(fsp, 'stat');
+            isFileStub = stub();
+            statsInstance = {
+                isFile: isFileStub
+            };
+        });
+        beforeEach(() => {
+            existsStub.reset();
+            statStub.reset();
+            isFileStub.reset();
+
+            existsStub.rejects(new Error('Stubbed function'));
+            statStub.rejects(new Error('Stubbed function'));
+            isFileStub.throws(new Error('Stubbed method'));
+        });
+        after(() => {
+            existsStub.restore();
+            statStub.restore();
+        });
+        it('should return `true` if the given path-like exists and is a regular file', async () => {
+            //-- Given
+            const p = fake.system.filePath();
+            existsStub.withArgs(p).resolves(true);
+            statStub.withArgs(p).resolves(statsInstance as fs.Stats);
+            isFileStub.returns(true);
+
+            //-- When
+            const r = await testModule.isFile(p);
+
+            //-- Then
+            expect(r).to.be.true;
+        });
+        it('should return `false` if the given path-like does not exist', async () => {
+            //-- Given
+            const p = fake.system.filePath();
+            existsStub.withArgs(p).resolves(false);
+            statStub.withArgs(p).resolves(statsInstance as fs.Stats);
+            isFileStub.returns(true);
+
+            //-- When
+            const r = await testModule.isFile(p);
+
+            //-- Then
+            expect(r).to.be.false;
+        });
+        it('should return `false` if the given path-like exists but is not a regular file', async () => {
+            //-- Given
+            const p = fake.system.filePath();
+            existsStub.withArgs(p).resolves(true);
+            statStub.withArgs(p).resolves(statsInstance as fs.Stats);
+            isFileStub.returns(false);
+
+            //-- When
+            const r = await testModule.isFile(p);
 
             //-- Then
             expect(r).to.be.false;
