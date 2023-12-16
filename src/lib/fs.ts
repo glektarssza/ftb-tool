0ec6b65c01e7ebc3a6b7532f49c82ca9e6277c49
G'lek Tarssza <glek@glektarssza.com>
2023-12-16 00:04:54 -0700

    Added file system item type check functions.

--- src/lib/fs.ts
+++ src/lib/fs.ts
@@ -37,6 +37,99 @@ const m = {
             return false;
         }
         return (await fsp.stat(p)).isFile();
+    },
+
+    /**
+     * Check if a given path-like exists on the file system and is a directory.
+     *
+     * @param p - The path-like to check.
+     *
+     * @returns `true` if the given path-like exists on the file system and is a
+     * directory; `false` otherwise.
+     */
+    async isDirectory(p: fs.PathLike): Promise<boolean> {
+        if (!(await m.exists(p))) {
+            return false;
+        }
+        return (await fsp.stat(p)).isDirectory();
+    },
+
+    /**
+     * Check if a given path-like exists on the file system and is a symbolic
+     * link.
+     *
+     * @param p - The path-like to check.
+     *
+     * @returns `true` if the given path-like exists on the file system and is a
+     * symbolic link; `false` otherwise.
+     */
+    async isSymbolicLink(p: fs.PathLike): Promise<boolean> {
+        if (!(await m.exists(p))) {
+            return false;
+        }
+        return (await fsp.stat(p)).isSymbolicLink();
+    },
+
+    /**
+     * Check if a given path-like exists on the file system and is a FIFO pipe.
+     *
+     * @param p - The path-like to check.
+     *
+     * @returns `true` if the given path-like exists on the file system and is a
+     * FIFO pipe; `false` otherwise.
+     */
+    async isFIFO(p: fs.PathLike): Promise<boolean> {
+        if (!(await m.exists(p))) {
+            return false;
+        }
+        return (await fsp.stat(p)).isFIFO();
+    },
+
+    /**
+     * Check if a given path-like exists on the file system and is a block
+     * device.
+     *
+     * @param p - The path-like to check.
+     *
+     * @returns `true` if the given path-like exists on the file system and is a
+     * block device; `false` otherwise.
+     */
+    async isBlockDevice(p: fs.PathLike): Promise<boolean> {
+        if (!(await m.exists(p))) {
+            return false;
+        }
+        return (await fsp.stat(p)).isBlockDevice();
+    },
+
+    /**
+     * Check if a given path-like exists on the file system and is a character
+     * device.
+     *
+     * @param p - The path-like to check.
+     *
+     * @returns `true` if the given path-like exists on the file system and is a
+     * character device; `false` otherwise.
+     */
+    async isCharacterDevice(p: fs.PathLike): Promise<boolean> {
+        if (!(await m.exists(p))) {
+            return false;
+        }
+        return (await fsp.stat(p)).isCharacterDevice();
+    },
+
+    /**
+     * Check if a given path-like exists on the file system and is a socket.
+     *
+     * @param p - The path-like to check.
+     *
+     * @returns `true` if the given path-like exists on the file system and is a
+     * socket; `false` otherwise.
+     */
+    async isSocket(p: fs.PathLike): Promise<boolean> {
+        if (!(await m.exists(p))) {
+            return false;
+        }
+        return (await fsp.stat(p)).isSocket();
     }
 };
 
