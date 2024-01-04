55cc027e1733c8cb45d063f1e852e5349829a977
G'lek Tarssza <glek@glektarssza.com>
2024-01-04 09:00:20 -0700

    Missed a doc comment.

--- src/lib/errors/argumentError.ts
+++ src/lib/errors/argumentError.ts
@@ -9,6 +9,13 @@ export class ArgumentError extends BaseError {
      */
     public readonly argumentName: string;
 
+    /**
+     * Create a new instance.
+     *
+     * @param argumentName - The name of the argument that was invalid.
+     * @param message - A string describing the error that occurred.
+     * @param inner - The error that caused the new instance to be thrown.
+     */
     public constructor(argumentName: string, message?: string, inner?: Error) {
         super(message ?? `Invalid argument "${argumentName}"`, inner);
         this.argumentName = argumentName;
