ddec55128b5709dea2d5a24c6e003f89ab6b0948
G'lek Tarssza <glek@glektarssza.com>
2024-02-09 11:29:02 -0700

    Added some unit tests for operation error class.

---
+++ tests/lib/errors/operationError.spec.ts
@@ -0,0 +1,80 @@
+//-- NPM Packages
+import {expect} from 'chai';
+import {en, en_US, base, Faker} from '@faker-js/faker';
+
+//-- Project Code
+import {OperationError} from '@src/lib/errors/operationError';
+
+//-- Test Utils
+import {parseOptionalEnvInteger} from '../../utils';
+
+/**
+ * The data faker instance for these tests.
+ */
+const fake = new Faker({
+    locale: [en, en_US, base]
+});
+
+describe('class:lib.errors.OperationError', () => {
+    before(() => {
+        console.debug(
+            `Using "${fake.seed(
+                parseOptionalEnvInteger('TESTS_FAKER_SEED') ?? undefined
+            )}" as random data seed for "class:lib.errors.OperationError" tests...`
+        );
+    });
+    describe('.constructor()', () => {
+        it('should set the `operationName` property to the given value', () => {
+            //-- Given
+            const operationName = fake.database.column();
+
+            //-- When
+            const r = new OperationError(operationName);
+
+            //-- Then
+            expect(r.operationName).to.equal(operationName);
+        });
+        it('should pass the `message` argument to the base class', () => {
+            //-- Given
+            const argumentName = fake.database.column();
+            const message = fake.lorem.sentence();
+
+            //-- When
+            const r = new OperationError(argumentName, message);
+
+            //-- Then
+            expect(r.message).to.equal(message);
+        });
+        it('should pass a default string to the base class as the `message` argument if none is given', () => {
+            //-- Given
+            const argumentName = fake.database.column();
+
+            //-- When
+            const r = new OperationError(argumentName);
+
+            //-- Then
+            expect(r.message).to.equal(`Invalid argument "${argumentName}"`);
+        });
+        it('should pass the `inner` argument to the base class', () => {
+            //-- Given
+            const argumentName = fake.database.column();
+            const inner = new Error();
+
+            //-- When
+            const r = new OperationError(argumentName, undefined, inner);
+
+            //-- Then
+            expect(r.inner).to.equal(inner);
+        });
+        it('should pass `undefined` to the because class as the `inner` argument to if none was given', () => {
+            //-- Given
+            const argumentName = fake.database.column();
+
+            //-- When
+            const r = new OperationError(argumentName);
+
+            //-- Then
+            expect(r.inner).to.be.undefined;
+        });
+    });
+});
