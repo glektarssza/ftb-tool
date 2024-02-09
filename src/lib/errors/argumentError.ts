39f575de88cbbe19d43306fc52c08bf537b71296
G'lek Tarssza <glek@glektarssza.com>
2024-02-09 07:58:08 -0700

    Forgot the rest of my template.

--- src/lib/errors/argumentError.ts
+++ src/lib/errors/argumentError.ts
@@ -22,3 +22,23 @@ export class ArgumentError extends BaseError {
         this.argumentName = argumentName;
     }
 }
+
+/**
+ * A module which provides custom error implementations for the application.
+ */
+const m = {};
+
+/**
+ * Get the internal module for unit testing.
+ *
+ * @returns The internal module.
+ *
+ * @internal
+ */
+export function getTestingModule(): typeof m {
+    return m;
+}
+
+/* eslint-disable no-empty-pattern, @typescript-eslint/unbound-method */
+export const {} = m;
+/* eslint-enable no-empty-pattern, @typescript-eslint/unbound-method */
